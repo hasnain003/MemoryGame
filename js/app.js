b7c95c182c3ef10e581ce2d51ef37179abd921a6
hasnain003 <mdhasnain839@gmail.com>
2018-08-08 19:01:05 +0530

    Added playAgain method and closing method for modal

--- js/app.js
+++ js/app.js
@@ -205,17 +205,29 @@ function end(){
 		{
 			clearInterval(interval);
 			const finalTime=timer.innerHTML;
-			const finalRating=document.querySelector('.stars');
+			timer.innerHTML=finalTime;
+			const finalRating=document.querySelector('.stars').innerHTML;
 			
 			modal.style.display = "block";
 			document.querySelector('.finalMoves').innerHTML=counter;
 			document.querySelector('.finalTime').innerHTML=finalTime;
 			document.querySelector('.rating').innerHTML=finalRating;
-			if(document.querySe)
-			closeModel();
+			
+			// When the user clicks on <span> (x), close the modal
+			let span= document.getElementsByClassName("close")[0];
+			span.onclick=function(){
+				modal.style.display="none";
+			}
+			
+			// When the user clicks anywhere outside of the modal, close it
+			window.onclick = function(event) {
+				if (event.target == modal) {
+					modal.style.display = "none";
+				}
+			}
 		}
 }
-
+/*
 function closeModel(){
 	// When the user clicks on <span> (x), close the modal
 	const span = document.getElementsByClassName("close")[0];
@@ -229,9 +241,10 @@ function closeModel(){
 			modal.style.display = "none";
     	}
 	}
-}
+} */
 
-function playGame(){
+function playAgain(){
+	console.log("Hello, I m in playAgain Section");
 	modal.style.display="none";
 	startGame();
 }
